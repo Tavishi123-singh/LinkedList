a46b17ca93d639f8a89c025979563873588a1297
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-11 23:41:06 +0530

    Final Commit

--- src/DoublyLinkedLIst/main/Main.java
+++ src/DoublyLinkedLIst/main/Main.java
@@ -1,4 +1,22 @@
 package DoublyLinkedLIst.main;
 
+import DoublyLinkedLIst.LinkedList.MyDoublyLinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        MyDoublyLinkedList obj=new MyDoublyLinkedList();
+        obj.insertAtBeginning(100);
+        obj.Print();
+        obj.insertAtBeginning(200);
+        obj.Print();
+        obj.insertAtLast(2000);
+        obj.insertAtLast(1000);
+        obj.Print();
+        obj.deleteFromBeginning();
+        obj.Print();
+        obj.deleteFromLast();
+        obj.Print();
+        obj.deleteFromBeginning();
+        obj.Print();
+    }
 }
