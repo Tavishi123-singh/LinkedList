6d1a492e8e57922314d40cceaccc917f63832b7a
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-11 23:19:49 +0530

    Final Commit

--- src/DoublyLinkedLIst/node/Node.java
+++ src/DoublyLinkedLIst/node/Node.java
@@ -1,5 +1,37 @@
 package DoublyLinkedLIst.node;
 
 public class Node {
+    private int data;
+    private Node next;
+    private Node previous;
 
+    public Node(int data) {
+        this.data = data;
+        next=null;
+        previous=null;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
 }
