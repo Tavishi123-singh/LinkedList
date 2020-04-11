017f01bf0c4a41f9beedcf3784acfe74c3a154ab
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-11 23:07:55 +0530

    final Commit

--- src/SinglyLinkedList/main/Main.java
+++ src/SinglyLinkedList/main/Main.java
@@ -1,4 +1,16 @@
 package SinglyLinkedList.main;
 
+import SinglyLinkedList.LinkedList.MySinglyLinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        MySinglyLinkedList obj=new MySinglyLinkedList();
+        obj.insertAtBeginning(100);
+        obj.insertAtBeginning(200);
+        obj.Print();
+        obj.insertAtLast(1000);
+        obj.Print();
+        obj.deleteFromBeginning();
+
+    }
 }
