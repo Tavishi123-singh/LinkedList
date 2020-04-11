40780a8a27b1ee6c758651622faaf4c4553c4506
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-11 23:33:16 +0530

    Final Commit

--- src/DoublyLinkedLIst/LinkedList/MyDoublyLinkedList.java
+++ src/DoublyLinkedLIst/LinkedList/MyDoublyLinkedList.java
@@ -1,5 +1,77 @@
 package DoublyLinkedLIst.LinkedList;
 
-public class MyDoublyLinkedList {
+import DoublyLinkedLIst.node.Node;
 
+public class MyDoublyLinkedList {
+    private Node head;
+    public MyDoublyLinkedList(){
+       head=null;
+    }
+    public void insertAtBeginning(int data){
+        Node node=new Node(data);
+        if(head==null){
+            node.setNext(head);
+            head=node;
+        }
+        else{
+            head.getPrevious(node);
+            node.setNext(head);
+            head=node;
+        }
+    }
+    public void insertAtLast(int data){
+        Node node=new Node(data);
+        if(head==null){
+            head=node;
+        }
+        else{
+            Node temp=head;
+            while(temp.getNext()!=null){
+                temp=temp.getNext();
+            }
+            node.setPrevious(temp);
+            temp.setNext(node);
+        }
+    }
+    public Node deleteFromBeginning(){
+        Node res=null;
+        if (head!=null){
+            res=head;
+            head=head.getNext();
+            head.setPrevious(null);
+        }
+        return res;
+    }
+    public Node deleteFromLast(){
+        Node res=null;
+        if(head!=null){
+            Node temp=head;
+            Node previous=null;
+            while (temp.getNext()!=null){
+                temp=temp.getNext();
+            }
+            if (temp.getPrevious()!=null){
+                res=temp;
+                temp=temp.getPrevious();
+                temp.setNext(null);
+            }
+            else{
+                res=head;
+                head=null;
+            }
+        }
+        return res;
+    }
+    public void Print(){
+        System.out.println("Printing List-------");
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.getData());
+            temp=temp.getNext();
+            if(temp!=null){
+                System.out.print("--->");
+            }
+        }
+        System.out.println();
+    }
 }
